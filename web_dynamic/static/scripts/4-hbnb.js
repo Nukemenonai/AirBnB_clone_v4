ea16eafeabe0674c0a45ad27417131aa76750aee
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-05-18 02:08:42 +0000

    [update]: files for task 5

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,77 @@
+const items = {};
+let aList = [];
+
+function listAppend () {
+  $('.amenities UL LI INPUT').css('margin-right', '10px');
+  $('input[type="checkbox"]').click(function () {
+    if ($(this).is(':checked')) {
+      aList.push($(this).parent().text());
+      item[$(this).attr('data-name')] = $(this).attr('data-id');
+    } else if ($(this).is(':not(:checked)')) {
+      aList.pop($(this).parent().text());
+      delete items[$(this).attr('data-name')];
+    }
+    console.log(aList);
+    $('.amenities h4').text(aList.join(', '));
+  });
+  checkStatus();
+  listing();
+}
+
+function checkStatus () {
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    success: function (data) {
+      console.log(data);
+      if (data['status'] === 'OK') {
+        $('DIV#api_status').addClass('available');
+      }
+    }
+  });
+}
+
+function listing () {
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    type: 'POST',
+    contentType: 'application/JSON',
+    data: JSON.stringify({ amenities: Object.values(items) }),
+    success: function (res) {
+      $.each(res, function (index, place){
+	$('SECTION.places').append(
+	  '<article>' +
+           '<div class="tittle">' +
+              '<h2>' + place.name +
+              '<div class="price_by_night">' +
+                  place.price_by_night +
+              '</div>' +
+	     '</h2>' +
+            '</div>' +
+            '<div class="information">' +
+	      '<div class="max_guest">' +
+		'<i class="fa fa-users fa-3x" aria-hidden="true"></i>' +
+		'<br />' +
+		   place.max_guest + ' Guests' +
+	     '</div>' +
+	      '<div class="number_rooms">' +
+		'<i class="fa fa-bed fa-3x" aria-hidden="true"></i>' +
+		'<br />' +
+		place.number_rooms + ' Bedrooms' +
+	     ' </div>' +
+	      '<div class="number_bathrooms">' +
+		'<i class="fa fa-bath fa-3x" aria-hidden="true"></i>' +
+		'<br />'+
+		place.number_bathrooms + ' Bathroom' +
+	      '</div>' +
+            '</div>' +
+	   '<div class="description">' +
+	      place.description +
+	   '</div>' +
+          '</article>'
+	);
+      });
+    }
+  });
+}
+
+$(document).ready(listAppend);
